3942d0cee7b815c504653fa84db94ff8652eff6f
HarshithaS2023 <135550507+HarshithaS2023@users.noreply.github.com>
2024-10-11 14:22:22 -0700

    Update Program7.java
    Updated Program7

--- src/main/java/Program7.java
+++ src/main/java/Program7.java
@@ -1,30 +1,42 @@
-package src.main.java;
 //import stuff here
-
+import java.util.Scanner;
 //Your code here
 
 public class Program7{
-	//Define your final variables here
-	// **HINT: Maybe the conversion rates?????**
-
-
-	public static void main(String[] args){
-		//Make you own test code here
-		Program7 tester = new Program7();
-		
-		double myTestVal = 0.0;
-		
-		myTestVal = tester.convertMoney(0,0,0);
-		
-		System.out.println(myTestVal);
-	}
-	
-	
-	
-	public double convertMoney(int schruteBucks, int stanleyNickels, int klevins){
-		//implement a function that converts from the old format to the new format
-		
-	}
-	
-	
+    public static void main(String[] args) {
+        double bucksOld = 0.0;
+        double klevinOld = 0.0;
+        double nicklesOld = 0.0;
+        
+        Scanner myScanner = new Scanner(System.in);
+        
+        System.out.print("Enter schrute-bucks: ");
+        bucksOld = myScanner.nextInt();
+        System.out.print("\nEnter klevins: ");
+        klevinOld = myScanner.nextInt();
+        System.out.print("\nEnter stanley-nickels: ");
+        nicklesOld = myScanner.nextInt();
+        
+        double bucksNew = 0;
+        final double klevinNew = klevinOld/20.0;
+        final double nickelsNew = nicklesOld/240.0;
+        
+        bucksNew = (double)Math.round((nickelsNew + klevinNew + bucksOld)*100)/100;
+        
+        System.out.println("\nDecimal schrute-bucks = $" + bucksNew);
+    }
 }
+//Paste console output below:
+/*
+
+Enter schrute-bucks: 7
+
+Enter klevins: 17
+
+Enter stanley-nickels: 9
+
+Decimal schrute-bucks = $7.89
+
+*/
+
+
